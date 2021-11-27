2bfaa7615567657eae3e4bc5c336f784f8e3d95a
matchless_nostril <80556770+Chan-sol@users.noreply.github.com>
2021-11-27 15:54:51 +0900

    modify login UI

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-content>
+    <v-content class="bgColor">
       <v-slide-x-transition mode="out in">
         <router-view></router-view>
       </v-slide-x-transition>
@@ -12,7 +12,12 @@ export default {
     name: 'App',
     created() {
         // 다크 모드
-        this.$vuetify.theme.dark = true;
+        // this.$vuetify.theme.dark = true;
     }
 }
 </script>
+<style>
+  .bgColor {
+    background-color: rgb(34, 34, 34);
+  }
+</style>
