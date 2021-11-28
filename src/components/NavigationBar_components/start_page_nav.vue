2e08d7f264e801b57fe16fcd975e644c4d0ca896
matchless_nostril <80556770+Chan-sol@users.noreply.github.com>
2021-11-29 03:42:25 +0900

    add scoped (to resolve gameSelect UI bug)

--- src/components/NavigationBar_components/start_page_nav.vue
+++ src/components/NavigationBar_components/start_page_nav.vue
@@ -22,13 +22,9 @@ export default {
     }
 }
 </script>
-<style>
+<style scoped>
   @import url('https://fonts.googleapis.com/css2?family=Oswald&display=swap');
 
-  .name {
-    font-weight: bolder;
-  }
-  
   .welcome {
     font-family: 'Oswald', sans-serif;
     font-size: 22px;
--- src/components/login_components/first_page.vue
+++ src/components/login_components/first_page.vue
@@ -54,7 +54,7 @@ export default {
     }
 }
 </script>
-<style>
+<style scoped>
     @import url(https://fonts.googleapis.com/css?family=BenchNine:700);
 
     @keyframes logoAnimation {
