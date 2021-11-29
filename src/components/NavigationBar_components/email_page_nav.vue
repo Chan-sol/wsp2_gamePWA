e4a40a967e97aed777c71be92419247d97ccdc66
matchless_nostril <80556770+Chan-sol@users.noreply.github.com>
2021-11-30 00:27:22 +0900

    email (login, register) navigation drawer UI & router

---
+++ src/components/NavigationBar_components/email_page_nav.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-list>
+        <v-list-item value="true" v-for="(item, i) in fnGetMenuItems" :to="item.to" :key="i">
+          <v-list-item-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-item-action>
+          <v-list-item-title v-text="item.title"></v-list-item-title>
+        </v-list-item>
+    </v-list>
+</template>
+<script>
+export default {
+    computed: {
+      fnGetAuthStatus() {
+        return this.$store.getters.fnGetAuthStatus
+      },
+      fnGetMenuItems() {
+        return [
+          {
+            title: 'Login Page',
+            to: '/first',
+            icon: 'mdi-launch'
+          }
+        ]
+      },
+    }
+}
+</script>
