ca62fab581052b9513bf2360804468a519b87798
matchless_nostril <80556770+Chan-sol@users.noreply.github.com>
2021-12-01 19:32:27 +0900

    feat (ranking) : ranking template initial

---
+++ src/components/ranking_components/ranking.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-container>
+        <h1>Ranking</h1>
+        <h2 v-model="score">{{ score }}</h2>
+    </v-container>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            gameName: '',
+            score: '',
+            rank: '',
+        }
+    }
+}
+</script>
